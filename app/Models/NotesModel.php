7c97b54816ac23e15cfeb70cbc147b65c8f85b4c
Yukino-Kitsune <kovtunov505@gmail.com>
2023-10-06 14:37:41 +0300

    Создал модель

---
+++ app/Models/NotesModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\NotesFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\table;
+
+class NotesModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notes';
+    protected $primaryKey = 'id';
+    protected $fillable = ['full_name', 'company', 'email', 'phone', 'photo'];
+
+    protected static function newFactory() : NotesFactory
+    {
+        return NotesFactory::new();
+    }
+
+    public static function getNotes() // TODO Сделать пагинацию
+    {
+        return DB::table('notes')->get();
+    }
+}
